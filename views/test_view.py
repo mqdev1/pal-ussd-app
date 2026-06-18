aaa6f694cf8f9d73ed3fad9753db5a96c726649b
Mahmoud Qassem <68344113+mqdev1@users.noreply.github.com>
2026-06-18 03:07:19 +0300

    Update test_view.py

--- views/test_view.py
+++ views/test_view.py
@@ -1,12 +1,11 @@
 
 import flet as ft
-
-def test_view(page: ft.Page):
-
-    return ft.View(
-        route='/test',
-        controls=[
-            ft.Text("This is test page"),
-            ft.ElevatedButton(text="Go Back", on_click=lambda _:page.go('/'))
-        ]
-    )
+from Router import Router
+def test_view(page:ft.Page, route:Router) -> ft.Control:
+    return ft.Column(
+            horizontal_alignment = ft.CrossAxisAlignment.CENTER,
+            controls=[
+                ft.Text("Page1", size=20, weight=ft.FontWeight.BOLD, color=ft.Colors.GREY),
+                ft.ElevatedButton(text="Back to home", on_click=lambda _:route.go_to("home_view"))
+            ]
+        )
